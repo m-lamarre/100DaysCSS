638f01095d3de745d0722df04e9684c1ef479fdc
m-lamarre <studium.square.pantz@gmail.com>
2018-03-26 17:58:02 +0200

    007 notifications search menu

---
+++ 007/007_notifications_search_menu.js
@@ -0,0 +1,8 @@
+$('.search-icon').bind('click', function() {
+	$('.search-input').toggleClass('active');
+});
+
+$('.menu-icon').bind('click', function() {
+	$('.center').toggleClass('show-menu');
+	$('.menu').toggleClass('active');
+});
