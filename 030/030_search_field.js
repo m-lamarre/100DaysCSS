5678ed7be613f9b98c5529342cb79894b841bbee
m-lamarre <mlamarre@student.wethinkcode.co.za>
2018-09-25 11:57:49 +0200

    030 search field

---
+++ 030/030_search_field.js
@@ -0,0 +1,17 @@
+$(".searchfield").keyup(function() {
+	let $suggestions = $(".suggestions");
+	let text = $(this).val();
+
+	if (text.length) {
+		$suggestions.html("").addClass("active");
+		$suggestions.append($("<li />", { html: "<b>" + text + "</b>" }));
+		$suggestions.append(
+			$("<li />", { html: "Veritatis et <b>" + text + "</b>" })
+		);
+		$suggestions.append(
+			$("<li />", { html: "ut aliquid ex <b>" + text + "</b> vero eos" })
+		);
+	} else {
+		$suggestions.html("").removeClass("active");
+	}
+});
